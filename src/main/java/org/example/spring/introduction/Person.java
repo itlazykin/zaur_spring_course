b7188b9d45ca19bfdee86c7a450db23b1d69072b
Denis Lazykin <dzaka89@gmail.com>
2025-05-17 11:54:14 +0300

    DI. С помощью сеттера.

--- src/main/java/org/example/spring/introduction/Person.java
+++ src/main/java/org/example/spring/introduction/Person.java
@@ -1,11 +1,40 @@
 package org.example.spring.introduction;
 
 public class Person {
-    private final Pet pet;
+    private Pet pet;
+    private String surname;
+    private int age;
+
+    public Person() {
+        System.out.println("Person bean is created");
+    }
 
     public Person(Pet pet) {
         System.out.println("Person bean is created");
         this.pet = pet;
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
+        this.pet = pet;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class Person: set surname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class Person: set age");
+        this.age = age;
     }
 
     public void callYourPet() {
