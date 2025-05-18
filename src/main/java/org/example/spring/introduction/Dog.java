b9d934f560f7c22244bf48124ef95c3caca4a95d
Denis Lazykin <dzaka89@gmail.com>
2025-05-18 14:05:33 +0300

    Аннотация @Scope

--- src/main/java/org/example/spring/introduction/Dog.java
+++ src/main/java/org/example/spring/introduction/Dog.java
@@ -1,5 +1,10 @@
 package org.example.spring.introduction;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
 public class Dog implements Pet {
     public Dog() {
         System.out.println("Dog bean is created");
